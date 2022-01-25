7b59b968c6f71102fbdffe05dc558afebc4a3915
lindseygrey <91283480+lindseygrey@users.noreply.github.com>
2022-01-25 16:21:58 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -9,11 +9,14 @@ function initMap() {
   // Note: The code uses the JavaScript Array.prototype.map() method to
   // create an array of markers based on a given "locations" array.
   // The map() method here has nothing to do with the Google Maps API.
-
+  const svgMarker = {
+    fillColor: "blue",
+   };
   const markers = locations.map((location, i) => {
     return new google.maps.Marker({
       position: location,
       label: labels[i % labels.length],
+      icon: svgMarker,
     });
   });
   // Add a marker clusterer to manage the markers.
